d620fdbd357865e9f9f92d19007e644b8ec3359d
Nastia Hien <nastia.parian@gmail.com>
2023-03-06 16:14:57 +0300

    ПР5

--- scripts/index.js
+++ scripts/index.js
@@ -1,25 +1,36 @@
-const editProfileButton = document.querySelector('.profile__edit-button'); // назначение переменной кнопки редактирования
-const editProfile = document.querySelector('.popup'); // назначение переменной попап
-const formElement = editProfile.querySelector('.popup__form'); // назначение переменной формы попап
-const userNameElement = document.querySelector('.profile__user-name'); // назначение переменной имя пользователя
-const userOccupationElement = document.querySelector('.profile__user-occupation'); // назначение переменной профессия
+//  попапы
+const editProfilePopup = document.querySelector(".popup__profile-edit"); // попап-редактор профиля
+const addPhotoPopup = document.querySelector(".popup__add-photo"); // попап-добавить карточку
 
-const userNameInput = editProfile.querySelector('.popup__input_user_name'); //назначение переменной поля ввода имя
-const occupationInput = editProfile.querySelector('.popup__input_user_occupation'); //назначение переменной поля ввода профессия
+//  кнопки
+const editProfileButton = document.querySelector(".profile__edit-button"); //  кнопка "редактировать профиль"
+const addPhotoButton = document.querySelector(".profile__add-button"); //  кнопка "добавить карточку"
+const closeProfileButton = editProfilePopup.querySelector(".popup__button-close-profile"); // кнопка закрытия формы "редактировать профиль"
+const closeAddPhotoButton = addPhotoPopup.querySelector(".popup__button-close-add"); // кнопка закрытия формы "добавить карточку"
 
-const closeProfileButton = editProfile.querySelector('.popup__button-close'); // назначение переменной кнопки закрытия формы
+// формы для ввода
+const formElementProfile = editProfilePopup.querySelector(".popup__form-profile"); //  форма "редактировать профиль"
 
-//отображает форму на странице, в поля формы по умолчанию назначает данные указанные в профиле
+//  имя пользователя и профессия на странице профиля
+const userNameElement = document.querySelector(".profile__user-name"); // имя пользователя
+const userOccupationElement = document.querySelector(".profile__user-occupation"); //  информация о пользователе
+
+// поля форм для ввода
+const userNameInput = editProfilePopup.querySelector(".popup__input_user_name"); // поле ввода имени
+const occupationInput = editProfilePopup.querySelector(".popup__input_user_occupation"); //поле ввода информации о пользователе
+
+// EDIT PROFILE BLOCK
+//Отображает форму редактирования профиля, в поля формы по умолчанию назначает данные указанные в профиле
 function openPopup() {
-  editProfile.classList.add('popup_opened');
+  editProfilePopup.classList.add("popup_opened");
   userNameInput.value = userNameElement.textContent;
   occupationInput.value = userOccupationElement.textContent;
 }
 
-// функция скрывает форму
+// функция скрывает форму редактирования профиля
 function closePopup() {
-  editProfile.classList.remove('popup_opened');
-};
+  editProfilePopup.classList.remove("popup_opened");
+}
 
 // функция задает данные профиля через форму и закрывает форму
 function handleFormSubmit(evt) {
@@ -27,24 +38,167 @@ function handleFormSubmit(evt) {
   userNameElement.textContent = userNameInput.value;
   userOccupationElement.textContent = occupationInput.value;
   closePopup();
-};
+}
 
-// создает событие, при нажатии на кнопку редактировать открывается форма
-editProfileButton.addEventListener('click', function () {
+// при нажатии на кнопку редактировать открывается форма "редактировать профиль"
+editProfileButton.addEventListener("click", function () {
   openPopup();
 });
 
-// создает событие, закрывает форму при нажатии на кнопку close
-closeProfileButton.addEventListener('click', function () {
+// при нажатии на кнопку добавить открывется форма "добавить фото"
+addPhotoButton.addEventListener("click", function () {
+  openAddPhotoPopup();
+});
+
+// закрывает форму "редактировать профиль" при нажатии на кнопку close (X)
+closeProfileButton.addEventListener("click", function () {
   closePopup();
 });
 
-//создает событие, данные введеные в форме, сохраняются в профиле и закрываются
-formElement.addEventListener('submit', handleFormSubmit);
+// закрывает форму "добавить фото" при нажатии на кнопку close (X)
+closeAddPhotoButton.addEventListener("click", function () {
+  closeAddPhotoPopup();
+});
 
+// данные введеные в форме "редактировать профиль" , сохраняются в профиле и закрываются
+formElementProfile.addEventListener("submit", handleFormSubmit);
 
+// ADD PHOTOCARD ELEMENT BLOCK
 
+// при нажатии на кнопку добавить открывает форму "добавить карточку"
+function openAddPhotoPopup() {
+  addPhotoPopup.classList.add("popup_opened");
+}
 
+// при нажатии на кнопку close (X) закрывает форму "добавить карточку"
+function closeAddPhotoPopup() {
+  addPhotoPopup.classList.remove("popup_opened");
+}
 
+// Массив изображений с заголовком
+const photoCards = [
+  {
+    title: "National Park, Australia",
+    image: "https://images.unsplash.com/photo-1548296404-93c7694b2f91?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+    alt: "National Park, Australia",
+  },
+  {
+    title: "San Francisco, CA, USA",
+    image: "https://images.unsplash.com/photo-1677629322752-7045c2c04b5b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=388&q=80",
+    alt: "San Francisco, CA, USA",
+  },
+  {
+    title: "New York, Brooklyn",
+    image: "https://images.unsplash.com/photo-1522083165195-3424ed129620?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1297&q=80",
+    alt: "New York, Brooklyn",
+  },
+  {
+    title: "Toronto, Canada",
+    image: "https://images.unsplash.com/photo-1588733103629-b77afe0425ce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+    alt: "Toronto, Canada",
+  },
+  {
+    title: "Seoul, South Korea",
+    image: "https://images.unsplash.com/photo-1562829612-55b71f529a49?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80",
+    alt: "Seoul, South Korea",
+  },
+  {
+    title: "Los Angeles, CA, USA",
+    image: "https://images.unsplash.com/photo-1647963945830-6c5e1c043f76?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1382&q=80",
+    alt: "Los Angeles, CA, USA",
+  },
+];
 
+const cardsContainer = document.querySelector(".gallery");
+console.log(cardsContainer);
+
+function createCard(card) {
+  const cardTemplate = document.querySelector("#card-template").content.cloneNode(true);
+  const cardImage = cardTemplate.querySelector(".gallery__image");
+  const cardTitle = cardTemplate.querySelector(".gallery__title");
+
+  cardImage.setAttribute("src", card.image);
+  cardImage.setAttribute("alt", card.alt);
+  cardTitle.textContent = card.title;
+
+  return cardTemplate;
+}
+
+function renderCard(card, cardsContainer) {
+  const cardTemplate = createCard(card);
+  cardsContainer.prepend(cardTemplate);
+}
+
+photoCards.forEach((card) => {
+  const cardElement = createCard(card);
+  cardsContainer.appendChild(cardElement);
+});
+
+// поля переменных элемента формы
+const formElement = document.querySelector(".popup__form-card");
+const nameImageElement = formElement.querySelector(".popup__input_image_name");
+const linkImageElement = formElement.querySelector(".popup__input_image_link");
+
+// Функция отправки формы 
+function handleFormSubmitCard(evt) {
+  evt.preventDefault();
+
+  // Получает значение вводимых элементов в поля формы
+  const titleValue = nameImageElement.value;
+  const imageValue = linkImageElement.value;
+
+  // Создает новую карточку объекта и добавляет в массив
+  const newCard = {
+    title: titleValue,
+    image: imageValue,
+    alt: "A new image",
+  };
+
+  photoCards.push(newCard);
+
+  // Отображает новую карточку в контейнере галереи
+  renderCard(newCard, cardsContainer);
+
+  // Сбрасывает форму
+  formElement.reset();
+  closeAddPhotoPopup();
+}
+
+// Добавляет слушатели в форму
+formElement.addEventListener("submit", handleFormSubmitCard);
+
+cardsContainer.addEventListener("click", function (evt) {
+  if (evt.target.classList.contains("gallery__button-like")) {
+    evt.target.classList.toggle("gallery__button-like_active");
+  } 
+  
+  else if (evt.target.classList.contains("gallery__bin-button")) {
+    evt.target.closest(".gallery__card").remove();
+  } 
+  
+  else if (evt.target.classList.contains("gallery__image")) {
+    zoomImage.src = evt.target.src;
+    zoomTitle.textContent = evt.target.alt;
+    openZoomPhotoPopup();
+  }
+});
+
+// Zoom
+const zoomPhotoPopup = document.querySelector(".popup__zoom-items");
+const zoomImage = document.querySelector(".popup__zoom-image");
+const zoomTitle = document.querySelector(".popup__zoom-title");
+
+function openZoomPhotoPopup() {
+  zoomPhotoPopup.classList.add("popup_opened");
+}
+
+function closeZoomPhotoPopup() {
+  zoomPhotoPopup.classList.remove("popup_opened");
+}
+
+const closeZoomButton = zoomPhotoPopup.querySelector(".popup__button-close-zoom-photo");
+
+closeZoomButton.addEventListener("click", function () {
+  closeZoomPhotoPopup();
+});
 
