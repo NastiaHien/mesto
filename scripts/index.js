d00cfa7039af014681fae65c2ec10571181a33e6
Nastia Hien <nastia.parian@gmail.com>
2023-03-07 22:05:35 +0300

    edit after code rewie

--- scripts/index.js
+++ scripts/index.js
@@ -20,98 +20,93 @@ const userOccupationElement = document.querySelector(".profile__user-occupation"
 const userNameInput = editProfilePopup.querySelector(".popup__input_user_name"); // поле ввода имени
 const occupationInput = editProfilePopup.querySelector(".popup__input_user_occupation"); //поле ввода информации о пользователе
 
+
+function openPopup(popupElement) {
+  popupElement.classList.add("popup_opened");
+}
+
+function closePopup(popupElement) {
+  popupElement.classList.remove("popup_opened");
+}
+
 // EDIT PROFILE BLOCK
 //Отображает форму редактирования профиля, в поля формы по умолчанию назначает данные указанные в профиле
-function openPopup() {
-  editProfilePopup.classList.add("popup_opened");
+function openProfilePopup() {
+  openPopup(editProfilePopup);
   userNameInput.value = userNameElement.textContent;
   occupationInput.value = userOccupationElement.textContent;
 }
 
-// функция скрывает форму редактирования профиля
-function closePopup() {
-  editProfilePopup.classList.remove("popup_opened");
-}
-
 // функция задает данные профиля через форму и закрывает форму
-function handleFormSubmit(evt) {
+function FormSubmitProfile(evt) {
   evt.preventDefault();
   userNameElement.textContent = userNameInput.value;
   userOccupationElement.textContent = occupationInput.value;
-  closePopup();
+  closePopup(editProfilePopup);
 }
 
 // при нажатии на кнопку редактировать открывается форма "редактировать профиль"
 editProfileButton.addEventListener("click", function () {
-  openPopup();
-});
-
-// при нажатии на кнопку добавить открывется форма "добавить фото"
-addPhotoButton.addEventListener("click", function () {
-  openAddPhotoPopup();
+  openProfilePopup();
 });
 
 // закрывает форму "редактировать профиль" при нажатии на кнопку close (X)
 closeProfileButton.addEventListener("click", function () {
-  closePopup();
+  closePopup(editProfilePopup);
+});
+
+// данные введеные в форме "редактировать профиль" , сохраняются в профиле и закрываются
+formElementProfile.addEventListener("submit", FormSubmitProfile);
+
+
+
+// ADD PHOTOCARD ELEMENT BLOCK
+
+// при нажатии на кнопку добавить открывется форма "добавить фото"
+addPhotoButton.addEventListener("click", function () {
+  openPopup(addPhotoPopup);
 });
 
 // закрывает форму "добавить фото" при нажатии на кнопку close (X)
 closeAddPhotoButton.addEventListener("click", function () {
-  closeAddPhotoPopup();
+  closePopup(addPhotoPopup);
 });
 
-// данные введеные в форме "редактировать профиль" , сохраняются в профиле и закрываются
-formElementProfile.addEventListener("submit", handleFormSubmit);
-
-// ADD PHOTOCARD ELEMENT BLOCK
-
-// при нажатии на кнопку добавить открывает форму "добавить карточку"
-function openAddPhotoPopup() {
-  addPhotoPopup.classList.add("popup_opened");
-}
-
-// при нажатии на кнопку close (X) закрывает форму "добавить карточку"
-function closeAddPhotoPopup() {
-  addPhotoPopup.classList.remove("popup_opened");
-}
-
-// Массив изображений с заголовком
+//Массив изображений с заголовком
 const photoCards = [
   {
     title: "National Park, Australia",
     image: "https://images.unsplash.com/photo-1548296404-93c7694b2f91?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
-    alt: "National Park, Australia",
+    // alt: "National Park, Australia",
   },
   {
     title: "San Francisco, CA, USA",
     image: "https://images.unsplash.com/photo-1677629322752-7045c2c04b5b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=388&q=80",
-    alt: "San Francisco, CA, USA",
+    // alt: "San Francisco, CA, USA",
   },
   {
     title: "New York, Brooklyn",
     image: "https://images.unsplash.com/photo-1522083165195-3424ed129620?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1297&q=80",
-    alt: "New York, Brooklyn",
+    // alt: "New York, Brooklyn",
   },
   {
     title: "Toronto, Canada",
     image: "https://images.unsplash.com/photo-1588733103629-b77afe0425ce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
-    alt: "Toronto, Canada",
+    // alt: "Toronto, Canada",
   },
   {
     title: "Seoul, South Korea",
     image: "https://images.unsplash.com/photo-1562829612-55b71f529a49?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80",
-    alt: "Seoul, South Korea",
+    // alt: "Seoul, South Korea",
   },
   {
     title: "Los Angeles, CA, USA",
     image: "https://images.unsplash.com/photo-1647963945830-6c5e1c043f76?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1382&q=80",
-    alt: "Los Angeles, CA, USA",
+    // alt: "Los Angeles, CA, USA",
   },
 ];
 
 const cardsContainer = document.querySelector(".gallery");
-console.log(cardsContainer);
 
 function createCard(card) {
   const cardTemplate = document.querySelector("#card-template").content.cloneNode(true);
@@ -136,9 +131,9 @@ photoCards.forEach((card) => {
 });
 
 // поля переменных элемента формы
-const formElement = document.querySelector(".popup__form-card");
-const nameImageElement = formElement.querySelector(".popup__input_image_name");
-const linkImageElement = formElement.querySelector(".popup__input_image_link");
+const formAddCard = document.querySelector(".popup__form-card");
+const nameImageElement = formAddCard.querySelector(".popup__input_image_name");
+const linkImageElement = formAddCard.querySelector(".popup__input_image_link");
 
 // Функция отправки формы 
 function handleFormSubmitCard(evt) {
@@ -155,52 +150,48 @@ function handleFormSubmitCard(evt) {
     alt: titleValue,
   };
 
-  photoCards.push(newCard);
-
   // Отображает новую карточку в контейнере галереи
   renderCard(newCard, cardsContainer);
 
   // Сбрасывает форму
-  formElement.reset();
-  closeAddPhotoPopup();
+  formAddCard.reset();
+  closePopup(addPhotoPopup);
 }
 
 // Добавляет слушатели в форму
-formElement.addEventListener("submit", handleFormSubmitCard);
+formAddCard.addEventListener("submit", handleFormSubmitCard);
 
 cardsContainer.addEventListener("click", function (evt) {
+
+  // добавление кнопки "лайк"
   if (evt.target.classList.contains("gallery__button-like")) {
     evt.target.classList.toggle("gallery__button-like_active");
   } 
   
+  // кнопка удаления карточки 
   else if (evt.target.classList.contains("gallery__bin-button")) {
     evt.target.closest(".gallery__card").remove();
   } 
   
   else if (evt.target.classList.contains("gallery__image")) {
-    zoomImage.src = evt.target.src;
-    zoomTitle.textContent = evt.target.alt;
-    openZoomPhotoPopup();
+    const cardTitle = evt.target.closest(".gallery__card").querySelector(".gallery__title").textContent;
+    const cardAlt = evt.target.getAttribute("alt");
+    zoomImage.setAttribute("src", evt.target.src);
+    zoomImage.setAttribute("alt", cardAlt);
+    zoomTitle.textContent = cardTitle;
+    openPopup(zoomPhotoPopup);
   }
 });
 
 // Zoom
 const zoomPhotoPopup = document.querySelector(".popup_zoom-items");
-console.log(zoomPhotoPopup);
 const zoomImage = document.querySelector(".popup__zoom-image");
 const zoomTitle = document.querySelector(".popup__zoom-title");
 
-function openZoomPhotoPopup() {
-  zoomPhotoPopup.classList.add("popup_opened");
-}
-
-function closeZoomPhotoPopup() {
-  zoomPhotoPopup.classList.remove("popup_opened");
-}
 
 const closeZoomButton = zoomPhotoPopup.querySelector(".popup__button-close-zoom-photo");
 
 closeZoomButton.addEventListener("click", function () {
-  closeZoomPhotoPopup();
+  closePopup(zoomPhotoPopup);
 });
 
